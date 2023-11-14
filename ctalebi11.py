6eee25a803e5242736223e21abe647b1c2c83de5
Cameron Talebi <cccmttt@me.com>
2023-11-14 00:31:51 +0000

    Adding my Lab Code

---
+++ ctalebi11.py
@@ -0,0 +1,5 @@
+#Code written by Cameron Talebi
+myNum = int(input("Enter a number: "))
+myNum = pow(myNum, 2)
+print("Your number squared is "+ str(myNum) +".\n When life gives you lemons, "
+	"make life take the lemons back! I dont want your damn lemons!")
